efee0ffa2e2063a0962e12d12861578f863088d0
liveseybooking-jpg <Liveseybooking@gmail.com>
2026-03-04 00:32:10 -0500

    hardcode supabase keys to test

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://mlgsmmkjlzskhknxuepd.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sZ3NtbWtqbHpza2hrbnh1ZXBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NTQwMTAsImV4cCI6MjA4ODEzMDAxMH0.dp8rzaDd6bjYUYUIDhz79ekBFLAoxNrXHa3k5HQWgO4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
